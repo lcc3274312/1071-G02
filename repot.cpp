c6eb81c11e2ad9569e15f65586d35522ada57bf1
lcc3274312 <43335022+lcc3274312@users.noreply.github.com>
2018-12-14 21:27:11 +0800

    Update repot.cpp

--- repot.cpp
+++ repot.cpp
@@ -39,76 +39,99 @@ int main()
 {
     int choice;
     int choice2;
-    float n;
+    int choice3;
+    int money;
+    printf("***********************\n");
+    printf("*     1.幣值轉換器    *\n");
+    printf("*     2.紀錄表        *\n");
+    printf("*     3.離開          *\n");
+    printf("***********************\n");
     while(1)
     {
-        printf("1.TWD\n");
-        printf("2.USD\n");
-        printf("3.EUR\n");
-        printf("4.EXIT\n");
-        printf("=>");
-        scanf("%d %d",&choice,&choice2);
-        if(choice==4)
+        printf("請輸入選項 :");
+        scanf("%d",&choice);
+        if(choice==3)
             break;
         switch(choice)
         {
         case 1:
-            if(choice2==4)
-                break;
+            printf("1.台幣    2.美金    3.歐元\n\n\n");
+            printf("請輸入欲轉換幣種 :___->____ : ");
+            scanf( "%d %d",&choice2,&choice3);
             switch(choice2)
             {
             case 1:
+                switch(choice3)
+                {
+                case 1:
+                    printf("請輸入欲轉換額度: ");
+                    scanf("%d",&money);
+                    printf("台幣轉台幣金額是: %d\n",money);
+                    printf("***********************\n");
+                    break;
+                case 2:
+                    printf("請輸入欲轉換額度: ");
+                    scanf("%d",&money);
+                    printf("台幣轉美金金額是: %.2f\n",TU(money));
+                    printf("***********************\n");
+                    break;
+                case 3:
+                    printf("請輸入欲轉換額度: ");
+                    scanf("%d",&money);
+                    printf("台幣轉歐元金額是: %.2f\n",TE(money));
+                    printf("***********************\n");
+                    break;
+                }
                 break;
             case 2:
-                printf("Enter n:");
-                scanf("%f",&n);
-                printf("%.2f TWD = %.2f USD\n",n,TU(n));
+                switch(choice3)
+                {
+                case 1:
+                    printf("請輸入欲轉換額度: ");
+                    scanf("%d",&money);
+                    printf("美金轉台幣金額是: %.2f\n",UT(money));
+                    printf("***********************\n");
+                    break;
+                case 2:
+                    printf("請輸入欲轉換額度: ");
+                    scanf("%d",&money);
+                    printf("美金轉美金金額是: %d\n",money);
+                    printf("***********************\n");
+                    break;
+                case 3:
+                    printf("請輸入欲轉換額度: ");
+                    scanf("%d",&money);
+                    printf("美金轉歐元金額是: %.2f\n",UE(money));
+                    printf("***********************\n");
+                    break;
+                }
                 break;
             case 3:
-                printf("Enter n:");
-                scanf("%f",&n);
-                printf("%.2f TWD = %.2f EUR\n",n,TE(n));
+                switch(choice3)
+                {
+                case 1:
+                    printf("請輸入欲轉換額度: ");
+                    scanf("%d",&money);
+                    printf("歐元轉台幣金額是: %.2f\n",ET(money));
+                    printf("***********************\n");
+                    break;
+                case 2:
+                    printf("請輸入欲轉換額度: ");
+                    scanf("%d",&money);
+                    printf("歐元轉美金金額是: %.2f\n",EU(money));
+                    printf("***********************\n");
+                    break;
+                case 3:
+                    printf("請輸入欲轉換額度: ");
+                    scanf("%d",&money);
+                    printf("歐元轉歐元金額是: %d\n",money);
+                    printf("***********************\n");
+                    break;
+                }
                 break;
             }
-            break;
-        case 2:
-            if(choice2==4)
-                break;
-            switch(choice2)
-            {
-            case 1:
-                printf("Enter n:");
-                scanf("%f",&n);
-                printf("%.2f USD = %.2f TWD\n",n,UT(n));
-                break;
-            case 2:
-                break;
-            case 3:
-                printf("Enter n:");
-                scanf("%f",&n);
-                printf("%.2f USD = %.2f EUR\n",n,UE(n));
-                break;
-            }
-            break;
-        case 3:
-            if(choice2==4)
-                break;
-            switch(choice2)
-            {
-            case 1:
-                printf("Enter n:");
-                scanf("%f",&n);
-                printf("%.2f EUR = %.2f TWD\n",n,ET(n));
-                break;
-            case 2:
-                printf("Enter n:");
-                scanf("%f",&n);
-                printf("%.2f EUR = %.2f USD\n",n,EU(n));
-                break;
-            case 3:
-                break;
-            }
-            break;
+                case 2:
+                    break;
         }
     }
 }
