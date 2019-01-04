73d1f99c9c602cacf39e035f0f8594804b493899
gangskiller <43244779+gangskiller@users.noreply.github.com>
2019-01-04 16:15:24 +0800

    Update functions.cpp

--- convert/functions.cpp
+++ convert/functions.cpp
@@ -1,1 +1,75 @@
+#include "convert.h"
 
+
+
+float TU(float n)
+
+{
+
+    float a;
+
+    a=n*0.0324;
+
+    return a;
+
+}
+
+float TE(float n)
+
+{
+
+    float a;
+
+    a=n*0.0285;
+
+    return a;
+
+}
+
+float UT(float n)
+
+{
+
+    float a;
+
+    a=n*30.805;
+
+    return a;
+
+}
+
+float UE(float n)
+
+{
+
+    float a;
+
+    a=n*0.88;
+
+    return a;
+
+}
+
+float ET(float n)
+
+{
+
+    float a;
+
+    a=n*34.83;
+
+    return a;
+
+}
+
+float EU(float n)
+
+{
+
+    float a;
+
+    a=n*1.14;
+
+    return a;
+
+}
