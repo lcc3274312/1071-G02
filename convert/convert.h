4d3c58f7fd3824762239f4cc192d6116e512cb8f
WJack611 <43244713+WJack611@users.noreply.github.com>
2019-01-04 12:02:05 +0800

    Create convert.h

---
+++ convert/convert.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 100
+
+struct Data;
+float TU(float n);
+float TE(float n);
+float UT(float n);
+float UE(float n);
+float ET(float n);
+float EU(float n);
+void record(Data *data[],int c2,int c3,float n,float b,int i);
+void pr(Data *data[],int n);
